a2e179b2d5128a0556e40c8fa0382f0ddd40bf70
StevenTheGeek <steventhegeek@gmail.com>
2024-02-13 01:47:48 +0000

    Switch to using secrets manager

--- variables.tf
+++ variables.tf
@@ -32,10 +32,6 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
-  type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
-}
 
 variable "tfe_organization" {
   type        = string
